39fe3d24458c649ccac57f5e24722f80236dc8e7
Xronik <xronik2000@gmail.com>
2021-04-06 17:52:00 +0300

    react deploy

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Button({ button, inputs }) {
   return (
-    <button type='submit'
-      onClick={() => { Object.values(button)[0](inputs[0], inputs[1]) }}>{Object.keys(button)}</button>
+    <button type='submit' onClick={() => {
+      Object.values(button)[0](inputs[0], inputs[1])
+    }}> {Object.keys(button)} </button>
 
   )
 }
