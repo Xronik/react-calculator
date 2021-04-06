03bba227d9934eebc95d49be54c7361773952fd0
Xronik <xronik2000@gmail.com>
2021-04-06 18:11:49 +0300

    route from App to Main

--- src/App.js
+++ src/App.js
@@ -1,58 +1,10 @@
-import styled from 'styled-components';
-import React, { useState } from 'react';
-import Button from './components/Button'
-import Inputs from './components/Inputs'
-import Result from './components/Result'
+import React from 'react';
+import Main from './components/Main'
 
 function App() {
-
-  const [count, setCount] = useState(0)
-  const [inputs, changeInputs] = useState([1, 1])
-
-  const button = {
-    btnValuesArr: [
-      { "+": (a, b) => { setCount(a + b) } },
-      { "-": (a, b) => { setCount(a - b) } },
-      { "*": (a, b) => { setCount(a * b) } },
-      { "/": (a, b) => { setCount(a / b) } },
-    ]
-  }
-
   return (
-    <Container>
-      <ButtonWrap>
-        <Button button={button.btnValuesArr[0]} inputs={inputs} />
-        <Button button={button.btnValuesArr[1]} inputs={inputs} />
-        <Button button={button.btnValuesArr[2]} inputs={inputs} />
-        <Button button={button.btnValuesArr[3]} inputs={inputs} />
-      </ButtonWrap>
-      <Inputs changeInputs={changeInputs} />
-      <Result count={count} />
-    </Container>
+    <Main />
   );
 }
-
-const ButtonWrap = styled.div`
-    display: grid;
-    grid-gap:20px;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    grid-area:buttons;
-    button {
-      width:60px;
-      height:60px;
-      font-size:24px;
-    }
-`
-
-const Container = styled.section`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: 200px repeat(2, 30px);
-    grid-auto-flow:column;
-    align-items: center;
-    justify-items:center;
-    grid-template-areas: "buttons buttons buttons buttons" ". inputs inputs ." ". result result .";
-`
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+import Button from './Button'
+import Inputs from './Inputs'
+import Result from './Result'
+
+function Main() {
+  const [count, setCount] = useState(0)
+  const [inputs, changeInputs] = useState([1, 1])
+
+  const button = {
+    btnValuesArr: [
+      { "+": (a, b) => { setCount(a + b) } },
+      { "-": (a, b) => { setCount(a - b) } },
+      { "*": (a, b) => { setCount(a * b) } },
+      { "/": (a, b) => { setCount(a / b) } },
+    ]
+  }
+
+  return (
+    <Container>
+      <ButtonWrap>
+        <Button button={button.btnValuesArr[0]} inputs={inputs} />
+        <Button button={button.btnValuesArr[1]} inputs={inputs} />
+        <Button button={button.btnValuesArr[2]} inputs={inputs} />
+        <Button button={button.btnValuesArr[3]} inputs={inputs} />
+      </ButtonWrap>
+      <Inputs changeInputs={changeInputs} />
+      <Result count={count} />
+    </Container>
+  );
+}
+
+const ButtonWrap = styled.div`
+    display: grid;
+    grid-gap:20px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-area:buttons;
+    button {
+      width:60px;
+      height:60px;
+      font-size:24px;
+    }
+`
+
+const Container = styled.section`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 200px repeat(2, 30px);
+    grid-auto-flow:column;
+    align-items: center;
+    justify-items:center;
+    grid-template-areas: "buttons buttons buttons buttons" ". inputs inputs ." ". result result .";
+`
+
+export default Main
